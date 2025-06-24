fd2e484a52bf1fc8d2ef229a0befb3f84ec4291b
achervets <achervets@gmail.com>
2025-06-24 16:23:10 -0700

    added search functionality from working directory

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
-import argparse
+import os, sys, argparse
 
 def main():
-    print("Hello World!")
+    '''
+    if len(sys.argv) > 1:
+        print("Hello, " + sys.argv[1] + " World!")
+    else:
+        print("Hello World!")
+    '''
+    file_search(sys.argv[1])
 
+def file_search(target):
+    cwd = os.getcwd()
+    for dirpath, dirnames, filenames in os.walk(cwd):
+        if target in filenames:
+            print(os.path.join(dirpath, target))
 
 
 
