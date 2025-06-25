b5e28b282124800b6359da26e32d2d1e78f70c18
achervets <achervets@gmail.com>
2025-06-25 16:27:12 -0700

    added --str searching in files

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def argument_parser():
     parser = argparse.ArgumentParser(description="this script performs basic find functions on local filesystem")
     parser.add_argument("--name", default="", help="the name of the file you are searching for (not absolute)")
     parser.add_argument("--dir", default=".", help="absolute directory to search in (default cwd)")
-    parser.add_argument("-a", action="store_false")
-    parser.add_argument("-r", action="store_false")
-    parser.add_argument("-f", action="store_false")
+    parser.add_argument("-a", action="store_false", help="include hidden directories in search")
+    parser.add_argument("-r", action="store_false", help="recursively check subdirectories")
+    parser.add_argument("-f", action="store_false", help="show absolute filepath for results")
+    parser.add_argument("--str", default="", help="only show files that contain given string")
     return parser.parse_args()
 
 # searches filesystem using os.walk()
@@ -34,6 +35,8 @@ def file_search(args):
             dirnames[:] = [d for d in dirnames if not d.startswith(".")]
         for name in filenames:
             if re.match(regex_pattern, name):
+                if not find_string(args.str, os.path.join(dirpath, name)):
+                    continue
                 if args.f:
                     file_list.append(os.path.join(dirpath, name))
                 else:
@@ -42,6 +45,16 @@ def file_search(args):
                 return file_list
     return file_list
 
+def find_string(str, file):
+    if str == "":
+        return True
+    if os.path.isfile(file):
+        with open(file, "r") as f:
+            for line in f:
+                if str in line:
+                    return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
